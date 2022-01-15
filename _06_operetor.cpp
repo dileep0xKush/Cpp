a8bd0ea214b3c576bf41fa2ee68e1594a83521f5
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-15 14:37:40 +0530

    Add files via upload

---
+++ _06_operetor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a =3;
+    int b =4;
+    cout<<"Operators in c++"<<endl;
+    cout<<"Following are the type in c++ - :) "<<endl;
+    //Arithmnetic Operetor 
+    // cout<<"The value of a+b is ="<<a+b<<e/ndl;
+    // cout<<"The value of a-b is ="<<a-b<<endl;
+    // cout<<"The value of a*b is ="<<a*b<<endl;
+    // cout<<"The value of a/b is ="<<a/b<<endl;
+    // cout<<"The value of a%b is ="<<a%b<<endl;
+    // return 0;
+
+    // comparison opreators
+    // cout<<"The value of a == b is = "<<(a==b)<<endl;
+    // cout<<"The value of a < b is = "<<(a<b)<<endl;
+    // cout<<"The value of a > b is = "<<(a>b)<<endl;
+    // cout<<"The value of a <= b is = "<<(a<=b)<<endl;
+    // cout<<"The value of a => b is = "<<(a>=b)<<endl;
+    // cout<<"The value of a != b is = "<<(a!=b)<<endl;
+    
+    // Logocal operators
+
+    // cout<<"The value of and operators a == b is =:  "<<((a==b) && (a<b))<<endl;
+    // cout<<"The value of or operators a == b is =:  "<<((a==b) || (a<b))<<endl;
+    // cout<<"The value of logocal not operators  a == b is =:  "<<(!(a==b))<<endl;
+
+    // assigmemts Operators
+    // int a=2;
+    // int b=23;
+    // char d='j';
+
+    
+} // namespace std;
