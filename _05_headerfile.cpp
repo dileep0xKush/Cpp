bdfc2b6fd5bc377360741c425d58a5a6db907d31
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-15 14:34:31 +0530

    Add files via upload

---
+++ _05_headerfile.cpp
@@ -0,0 +1,12 @@
+// 1.there are two type file : it comes with the _EDG_COMPILER
+#include<iostream>
+// 2. user defined header files : it is  written by the programmer
+#include "this.h";//this any file 
+using namespace std;
+int main(){
+    cout<<"hello";
+    return 0 ;
+}
+
+// header file reference website -cpprefrence
+//  https://en.cppreference.com/w/cpp/header
