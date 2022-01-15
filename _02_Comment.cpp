d5fc38b8c32b9938800745fcc1580d193d4cbb0a
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-15 12:04:11 +0530

    Add files via upload

---
+++ _02_Comment.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+        // single line comment
+
+    //  this is a program
+
+// multiline __cpp_lib_incomplete_container_elements 
+
+/*
+this 
+is 
+a 
+first 
+program
+*/
+    cout<<"Comments";
+    return 0;
+}
