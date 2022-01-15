ef62f8b7bcfe690da1673bfc72ba35a8dd621a37
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-15 15:28:06 +0530

    Add files via upload

---
+++ _07_RefrenceVariable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float d=34.2;
+    long double e = 34.2;
+     cout<<"Float Value d ="<<d<<endl<<"double value is ="<<e;
+   
+   
+    return 0;
+
+}
