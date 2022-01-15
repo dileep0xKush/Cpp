9eefba94b6204ab991b8dabe317c87e574c92d1b
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-15 14:42:20 +0530

    Add files via upload

---
+++ _03_variable.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+// Golabal variable 
+int a=0;
+
+void sum(){
+        // local variable
+        int a;
+
+    }
+int main(){
+    // Local Variable
+    int a=2;
+    int b=34;
+    float p =2.32;
+    char c  = 'u';
+    cout<<"This is value of a = "<<a<<endl<<"This is value of b = "<<b<<endl;
+    cout<<"The  is value of p = "<<p<<endl;
+    cout<<"The  is value of c = "<<c;
+    return 0;
+}
