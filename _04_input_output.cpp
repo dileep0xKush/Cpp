5c03e8921c7a7462efedeb937aef30c32c99236f
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-15 14:42:36 +0530

    Add files via upload

---
+++ _04_input_output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"enter number of a = "<<endl;
+    cin>>a;
+    cout<<"enter number of b = "<<endl;
+    cin>>b;
+    cout<<"sum of a and b = "<< a+b<<endl;
+    return 0;
+}
