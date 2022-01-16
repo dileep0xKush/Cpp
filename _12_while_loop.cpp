2a3be805e4cfc141b1afa854a6c0a89458b30787
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 12:27:26 +0530

    Add files via upload

---
+++ _12_while_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int i=0;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    while (i<=n)
+    {
+       cout<<i<<endl;
+       i++; /* code */
+    }
+    
+}
