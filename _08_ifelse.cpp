9a96d22ca841584086f9f54c6cc4b11777c8c977
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 11:36:28 +0530

    Add files via upload

---
+++ _08_ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter your age "<<endl;
+    cin>>age;
+    if(age<18){
+        cout<<"you can not come party ";
+    }
+    else{
+        cout<<"you can come to the party";
+
+    }
+    return 0;
+}
