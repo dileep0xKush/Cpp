3ceb9cb69f867a23769264c398cf62959bfed7fc
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 12:26:41 +0530

    Add files via upload

---
+++ _11_for_loop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // print number 0 to 10
+
+    // for (int  i = 0; i < 10; i++)
+    // {
+    //    cout<<i<<endl; /* code */
+    // }
+
+
+    // user number input 
+    int n;
+    cout<<"enter a number ";
+    cin>>n;
+    for (int  i = 0; i < n; i++)
+    {
+        cout<<i<<endl;
+        /* code */
+    }
+    
+
+    return 0;
+}
