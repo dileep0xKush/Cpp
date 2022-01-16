11fc7af01235c8aa5bb534c804c07bac45b2a9c9
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 12:28:30 +0530

    Add files via upload

---
+++ _14_Break_Continue.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for (int i = 0; i < 10; i++)
+    // {
+    //    
+    //     if (i==2)
+    //     {
+    //         break;  
+    //     }
+    //  cout<<i<<endl;
+    // }
+    
+      for (int i = 0; i < 10; i++)
+    {
+       
+        if (i==2)
+        {
+           continue;  
+             
+        }
+        cout<<i<<endl;
+       
+    }
+    return 0;
+}
