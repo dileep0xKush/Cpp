0cacd3421f623c59496a5e511262c6b285dbc32b
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 11:37:20 +0530

    Add files via upload

---
+++ _09_elseif.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter  value  "<<endl;
+    cin>>a;
+    if(a<10){
+        cout<<"Ram"<<endl;
+
+    }
+    else if(a>10){
+        cout<<"rahul"<<endl;
+    }
+    else{
+        cout<<"ramesh"<<endl;
+    }
+    return 0;
+}
