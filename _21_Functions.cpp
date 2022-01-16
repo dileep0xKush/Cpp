b2537c32080132e03554c90f9b896c01637990e9
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 23:14:07 +0530

    Add files via upload

---
+++ _21_Functions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+    int c =a+b;
+    
+}
+
+int main(){
+
+int num1,num2;
+cout<<"Enter two number = "<<endl;
+cin>>num1>>num2;
+
+cout<<"Sum is  = "<<sum(num1,num2);
+
+
+    return 0;
+
+}
