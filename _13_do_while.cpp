23b3c288c3223f3de38c7a13ff7117c9be5e3ae2
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-16 12:28:09 +0530

    Add files via upload

---
+++ _13_do_while.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    // do
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    //     /* code */
+    // } while (i<=40);
+    // return 0;
+
+    do
+    {
+        cout<<i<<endl;
+        i++;
+        /* code */
+    } while (false);
+    
+    
+
+
+}
