e74d482c59b8875b8a1314d9a9af67149dc98de4
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-17 22:38:20 +0530

    Add files via upload

---
+++ _23_inline _Functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+inline int sum(int a, int b)
+{
+    return = a * b;
+}
+int main()
+{
+    int a, b;
+    cout << "enter  number  a and b " << endl;
+    cin >> a >> b;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    cout << "the products  of 4 and 5 = " << sum(a, b) << endl;
+    return 0;
+}
