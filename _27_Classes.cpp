a747ccd7b1c0925306225cd3eba07344af387937
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-18 15:42:17 +0530

    Add files via upload

---
+++ _27_Classes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class employee
+{
+private:
+    int a, b, c;
+
+public:
+    int d, e;
+    void setData(int a, int b, int c);
+    void getData()
+    {
+        cout << "The value of a is = " << a << endl;
+        cout << "The value of b is = " << b << endl;
+        cout << "The value of c is = " << c << endl;
+        cout << "The value of d is = " << d << endl;
+        cout << "The value of e is = " << e << endl;
+    }
+};
+
+void employee ::setData(int a1, int b1, int c1)
+{
+    a = a1;
+    b = b1;
+    c = c1;
+}   
+int main()
+{
+    employee ram;
+    ram.d = 34;
+    ram.e = 36;
+    ram.setData(1, 2, 3);
+    ram.getData();
+    return 0;
+}
